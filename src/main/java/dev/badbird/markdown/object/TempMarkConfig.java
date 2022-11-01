ef631ac5d26cf96ca3f16d6b50ac4e4aa4dc1ea8
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-01 17:26:04 -0400

    remove builder

--- src/main/java/dev/badbird/markdown/object/TempMarkConfig.java
+++ src/main/java/dev/badbird/markdown/object/TempMarkConfig.java
@@ -9,49 +9,5 @@ public class TempMarkConfig {
     private boolean useNewParserForNestedTemplates = true;
     private boolean replaceGithubURL = true; // replace github.com with raw.githubusercontent.com
     public TempMarkConfig() {
-
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private boolean cacheResults;
-        private boolean parseNestedTemplates;
-        private boolean useNewParserForNestedTemplates;
-        private boolean replaceGithubURL;
-
-        Builder() {
-        }
-
-        public Builder cacheResults(boolean cacheResults) {
-            this.cacheResults = cacheResults;
-            return this;
-        }
-
-        public Builder parseNestedTemplates(boolean parseNestedTemplates) {
-            this.parseNestedTemplates = parseNestedTemplates;
-            return this;
-        }
-
-        public Builder useNewParserForNestedTemplates(boolean useNewParserForNestedTemplates) {
-            this.useNewParserForNestedTemplates = useNewParserForNestedTemplates;
-            return this;
-        }
-
-        public Builder replaceGithubURL(boolean replaceGithubURL) {
-            this.replaceGithubURL = replaceGithubURL;
-            return this;
-        }
-
-        public TempMarkConfig build() {
-            TempMarkConfig tempMarkConfig = new TempMarkConfig();
-            tempMarkConfig.setCacheResults(cacheResults);
-            tempMarkConfig.setParseNestedTemplates(parseNestedTemplates);
-            tempMarkConfig.setUseNewParserForNestedTemplates(useNewParserForNestedTemplates);
-            tempMarkConfig.setReplaceGithubURL(replaceGithubURL);
-            return tempMarkConfig;
-        }
     }
 }
