a8258aa712e4563df88075e3fa9027c201dcfd1d
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-01 15:22:29 -0400

    remove tests because it was messing up stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("com.google.guava:guava:31.1-jre")
 }
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+/*
 val sourcesJar by tasks.registering(Jar::class) {
     archiveClassifier.set("sources")
     from(sourceSets["main"].allSource)
@@ -31,17 +26,18 @@ val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
     from(tasks["javadoc"])
 }
-tasks {
-    shadowJar {
-        archiveClassifier.set("")
-        exclude("org.checkerframework.*")
-        relocate("com.google", "dev.badbird.markdown.relocated.google")
-    }
-}
 artifacts {
     add("archives", sourcesJar)
     add("archives", javadocJar)
 }
+ */
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("com.google", "dev.badbird.markdown.relocated.google")
+    }
+}
+
 publishing {
     repositories {
         maven {
